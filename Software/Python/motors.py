ee86c9e1984ec2accfe3179bd50365fd3a834594
addy999 <addy@loftorbital.com>
2019-11-03 19:14:07 -0500

    individual functions made for directions

--- Software/Python/motors.py
+++ Software/Python/motors.py
@@ -1,10 +1,14 @@
-def moveStraight(motor_a_coeff=1.0, motor_b_coeff=1.0,  speed = 1.0, direction = 1):
-    ''' a,b,c '''
+def moveFwd(motor_a_coeff=1.0, motor_b_coeff=1.0, speed = 1.0):
+    ''' straight by default'''
     return [
-        255 * speed * direction * motor_a_coeff,
-        -255 * speed * direction * motor_b_coeff,
+        -255 * speed * motor_a_coeff,
+        255 * speed * motor_b_coeff,
         0.0
     ]
+    
+def moveBwd(motor_a_coeff=1.0, motor_b_coeff=1.0, speed = 1.0):
+    ''' straight by default'''
+    return moveFwd(motor_a_coeff, motor_b_coeff, speed) * -1
 
 def slideLeft(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed = 1.0):
     ''' a,b,c '''
@@ -16,12 +20,14 @@ def slideLeft(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed = 1
 
 def slideRight(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed = 1.0):
     ''' a,b,c '''
-    return slideLeft(motor_a_coeff, motor_b_coeff, motor_c_coeff, speed) * -1
+    return [i * -1 for i in slideLeft(motor_a_coeff, motor_b_coeff, motor_c_coeff, speed)]
 
-def turn(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed=1.0, direction=1):
-    ''' a,b,c '''
+def turnLeft(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed=1.0):
     return [
-        255 * speed * direction * motor_a_coeff, 
-        255 * speed * direction * motor_b_coeff, 
-        255 * speed * direction * motor_c_coeff
+        255 * speed * motor_a_coeff, 
+        255 * speed * motor_b_coeff, 
+        255 * speed * motor_c_coeff
     ]
+    
+def turnRight(motor_a_coeff=1.0, motor_b_coeff=1.0, motor_c_coeff=1.0, speed=1.0):
+    return [i*-1 for i in turnLeft(motor_a_coeff, motor_b_coeff, motor_c_coeff, speed)]
