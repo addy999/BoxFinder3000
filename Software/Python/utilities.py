cd4191d429a0721a6d8843f63edb006f49d0915a
addy999 <addy@loftorbital.com>
2019-11-03 19:13:52 -0500

    sleep added to commands

--- Software/Python/utilities.py
+++ Software/Python/utilities.py
@@ -8,21 +8,23 @@ def startBT(port):
     print(ser.name, 'Open: ', ser.isOpen())
     return ser
 
-def commandToStr(command = list):
+def commandToStrBytes(command = list, to_add = ""):
     
-    str_list = ''.join(str(e) for e in command)
+    str_list = ','.join(str(e) for e in command)
+    str_list += to_add
     str_list += "\n"
     return bytes(str_list, 'utf-8')
 
-def sendRxCommand(ser, command, to_print=False):
-    if to_print:
-        print('Sending')
-    ser.write(commandToStr(command));
+def sendRxCommand(ser, motor_command, sleep_req_ms=0.0, offset_s = 0.1):
     
-    sleep(0.1)
+    # Sending
+    str_command = commandToStrBytes(motor_command, '-' + str(sleep_req_ms))
+    print('Sending', str_command)
+    ser.write(str_command)
     
-    if to_print:
-        print('Reading...')
+    sleep(sleep_req_ms / 1000 + offset_s) # plus offset to allow comms to come in
+    
+    # Reading
     a = str(ser.readline())
     if len(a) > 20:
         a = a[2:-2]
@@ -30,11 +32,6 @@ def sendRxCommand(ser, command, to_print=False):
         a = a.replace("\n", "")
         a = a.replace("  ", "")[:-1]
         a = [float(c) for c in a.split("-")]
-        print(a)
         return a
-
-# print('Writing...')
-# ser.write(b'hello\n')
-# print('wrote')
-# print('Response', str(ser.readline()))
-
+    else:
+        return None
